e33ee48f9a914144e03c592f52901f581b05245b
JacekRaczynski <jacek.raczynski97@gmail.com>
2025-08-11 12:54:25 -0400

    fixes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -16,11 +16,12 @@ class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
+
     def __str__(self):
         return self.name  # Return the name as the string representation
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
